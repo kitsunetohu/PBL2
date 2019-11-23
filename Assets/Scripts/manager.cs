98eb55e1f4631228d7ad69d1c1f7a72976be06c5
kitsunetohu <bofenghui@gmail.com>
2019-11-23 21:38:27 +0900

    add lineDrawer

---
+++ Assets/Scripts/manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager : MonoBehaviour
+{
+    public DrawLine drawLine;
+    public Transform endPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKey(KeyCode.Space)){
+            drawLine.UpdateLine(endPoint.position);
+        }else{
+            drawLine.EndDraw();
+        }
+    }
+}
